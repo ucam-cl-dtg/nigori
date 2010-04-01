55ebf817edc7a3b58d4c120dbd365fd3d7e9cf47
Ben Laurie <ben@links.org>
2010-04-01 17:03:58 +0100

    More test vectors.

--- server/pbkdf2.py
+++ server/pbkdf2.py
@@ -47,6 +47,30 @@ def main():
               "4b007901b765489abead49d926f721d065a429c1" ],
 #            [ "password", "salt", 16777216, 20,
 #              "eefe3d61cd4da4e4e9945b3d6ba2158c2634e984" ],
+            
+            # Test vectors from RFC 3962
+            [ "password", "ATHENA.MIT.EDUraeburn", 1, 16,
+              "cdedb5281bb2f801565a1122b2563515" ],
+            [ "password", "ATHENA.MIT.EDUraeburn", 2, 16,
+              "01dbee7f4a9e243e988b62c73cda935d" ],
+            [ "password", "ATHENA.MIT.EDUraeburn", 1200, 16,
+              "5c08eb61fdf71e4e4ec3cf6ba1f5512b" ],
+            [ "password", int2bin(int("1234567878563412", 16)), 5, 16,
+              "d1daa78615f287e6a1c8b120d7062a49" ],
+            [ "X" * 64, "pass phrase equals block size", 1200, 16,
+              "139c30c0966bc32ba55fdbf212530ac9" ],
+            [ "X" * 65, "pass phrase exceeds block size", 1200, 16,
+              "9ccad6d468770cd51b10e6a68721be61" ],
+            [ int2bin(int("f09d849e", 16)), "EXAMPLE.COMpianist", 50, 16,
+              "6b9cf26d45455a43a5b8bb276a403b39", ],
+
+            # Test vectors from
+            # http://www.cryptosys.net/manapi/api_PBE_Kdf2.html
+            [ "password", int2bin(int("78578e5a5d63cb06", 16)), 2048, 24,
+              "bfde6be94df7e11dd409bce20a0255ec327cb936ffe93643" ],
+            [ "password", int2bin(int("78578e5a5d63cb06", 16)), 2048, 64,
+              "bfde6be94df7e11dd409bce20a0255ec327cb936ffe93643c4b150def77511224479994567f2e9b4e3bd0df7aeda3022b1f26051d81505c794f8940c04df1144" ],
+
           ]
 
   fails = 0
