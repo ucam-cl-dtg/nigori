95d2c109119932364a89a0048d28662a48d4ce5c
Daniel Thomas <drt24@cam.ac.uk>
2011-10-24 10:16:33 +0100

    Fix RegistartionTest: use maven gae to start and stop dev server
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- client/src/test/java/com/google/nigori/client/RegistrationTest.java
+++ client/src/test/java/com/google/nigori/client/RegistrationTest.java
@@ -18,13 +18,11 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.google.appengine.tools.development.DevAppServer;
-import com.google.appengine.tools.development.DevAppServerFactory;
 
 /**
  * @author drt24
@@ -32,30 +30,75 @@ import com.google.appengine.tools.development.DevAppServerFactory;
  */
 public class RegistrationTest {
 
-  public static final int PORT = 5005;
+  public static final int PORT = 8080;
   public static final String HOST = "localhost";
-  private static DevAppServer server;
+  private static Thread starter;
+  private static long startTime;
+  private static final int EXTERNAL_TIMEOUT = 10000;
 
+  protected static class GaeThread extends Thread {
+    private final String gaeCommand;
+    public GaeThread(String command){
+      gaeCommand = command;
+    }
+    public void run() {
+
+      try {
+        String cmd = "mvn gae:" + gaeCommand; // this is the command to execute in the
+                                                      // Unix shell
+        // create a process for the shell
+        ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
+
+        File current = new File("").getAbsoluteFile();
+        File parent = current.getParentFile();
+        pb.directory(new File(parent, "server"));
+        pb.redirectErrorStream(true); // use this to capture messages sent to stderr
+        Process shell = pb.start();
+        InputStream shellIn = shell.getInputStream(); // this captures the output from the command
+        int shellExitStatus = shell.waitFor(); // wait for the shell to finish and get the return
+                                               // code
+        // at this point you can process the output issued by the command
+        // for instance, this reads the output and writes it to System.out:
+        System.out.println(shellExitStatus);
+        int c;
+        while ((c = shellIn.read()) != -1) {
+          System.out.write(c);
+        }
+        // close the stream
+        shellIn.close();
+      } catch (IOException ignoreMe) {
+        ignoreMe.printStackTrace();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
   @BeforeClass
   public static void startDevServer() throws Exception {
-    server = new DevAppServerFactory().createDevAppServer(new File("war"), HOST, PORT);
-    server.start();
+    startTime = System.currentTimeMillis();
+    starter = new GaeThread("start");
+    starter.start();
+    starter.join(EXTERNAL_TIMEOUT);
   }
 
   @AfterClass
   public static void stopDevServer() throws Exception {
-    server.shutdown();
+    starter.join(EXTERNAL_TIMEOUT);
+    Thread stopper = new GaeThread("stop");
+    stopper.start();
+    stopper.join(EXTERNAL_TIMEOUT);
+    System.out.println("Total time (ms): " + ((System.currentTimeMillis() - startTime)));
   }
 
   @Test
   public void test() throws NigoriCryptographyException, IOException {
-    NigoriDatastore nigori = new NigoriDatastore(HOST, PORT, "");
+    NigoriDatastore nigori = new NigoriDatastore(HOST, PORT, "nigori");
     for (int i = 0; i < 3; ++i) {// check we can do this more than once
-      assertTrue(nigori.register());
-      assertTrue(nigori.authenticate());
-      assertTrue(nigori.unregister());
-      assertFalse(nigori.authenticate());
-      assertFalse(nigori.unregister());
+      assertTrue("Not registered", nigori.register());
+      assertTrue("Can't authenticate",nigori.authenticate());
+      assertTrue("Can't unregister", nigori.unregister());
+      assertFalse("Authenticated after unregistration", nigori.authenticate());
+      assertFalse("Could re-unregister", nigori.unregister());
     }
   }
 
---
+++ common/src/test/java/com/google/nigori/common/FailingCommonTests.java
@@ -0,0 +1,15 @@
+package com.google.nigori.common;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Tests which are expected to fail for common
+ * @author drt24
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses ({SchnorrSignTest.class})
+public class FailingCommonTests {
+
+}
