5c7821feaa0f1bf290ed640705d10e802e10332e
Daniel Thomas <drt24@cam.ac.uk>
2011-11-16 11:43:18 +0000

    Registration acceptance test passes
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- client/src/test/java/com/google/nigori/client/ClientTests.java
+++ client/src/test/java/com/google/nigori/client/ClientTests.java
@@ -3,13 +3,18 @@ package com.google.nigori.client;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.google.nigori.client.accept.RegistrationTest;
+
 /**
  * Tests which are expected to pass for client
  * @author drt24
  *
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses ({KeyManagerTest.class})
+@Suite.SuiteClasses ({
+  KeyManagerTest.class,
+  RegistrationTest.class
+  })
 public class ClientTests {
 
 }
--- client/src/test/java/com/google/nigori/client/FailingClientTests.java
+++ client/src/test/java/com/google/nigori/client/FailingClientTests.java
@@ -3,15 +3,13 @@ package com.google.nigori.client;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import com.google.nigori.client.accept.RegistrationTest;
-
 /**
  * Tests which are expected to fail for client
  * @author drt24
  *
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses ({RegistrationTest.class})
+@Suite.SuiteClasses ({})
 public class FailingClientTests {
 
 }
