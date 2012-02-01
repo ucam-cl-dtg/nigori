b29fd26b8178b9f87a4430c632670a28ca9492be
Daniel Thomas <drt24@cam.ac.uk>
2012-02-01 11:53:33 +0000

    Ensure je database is deleted after tests run
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- server/src/test/java/com/google/nigori/server/JEDatabaseTest.java
+++ server/src/test/java/com/google/nigori/server/JEDatabaseTest.java
@@ -17,6 +17,8 @@ package com.google.nigori.server;
 
 import java.io.File;
 
+import org.junit.AfterClass;
+
 /**
  * @author drt24
  * 
@@ -25,9 +27,40 @@ public class JEDatabaseTest extends AbstractDatabaseTest {
 
   @Override
   protected Database getDatabase() {
-    File dataDir = new File("jedir/");// TODO(drt24) do something better with dataDir
+    File dataDir = new File("je-test-dir/");
     dataDir.mkdir();
     return new JEDatabase(dataDir);
   }
 
+  @AfterClass
+  public static void deleteDatabase() {
+    File dataDir = new File("je-test-dir/");
+    if (dataDir.exists()){
+      deleteDir(dataDir);
+    }
+  }
+
+  /**
+   * Deletes all files and subdirectories under dir. Returns true if all deletions were successful.
+   * If a deletion fails, the method stops attempting to delete and returns false.
+   * 
+   * From http://www.exampledepot.com/egs/java.io/DeleteDir.html
+   * 
+   * @param dir
+   * @return
+   */
+  public static boolean deleteDir(File dir) {
+    if (dir.isDirectory()) {
+      String[] children = dir.list();
+      for (int i = 0; i < children.length; i++) {
+        boolean success = deleteDir(new File(dir, children[i]));
+        if (!success) {
+          return false;
+        }
+      }
+    }
+
+    // The directory is now empty so delete it
+    return dir.delete();
+  }
 }
